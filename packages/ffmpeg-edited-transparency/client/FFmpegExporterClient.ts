af910f2f6b6a92f99989085e1bfb37f32f89462d
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2023-05-16 19:15:45 +0100

    allowed for more options when rendering, also formatted code

--- packages/ffmpeg-edited-transparency/client/FFmpegExporterClient.ts
+++ packages/ffmpeg-edited-transparency/client/FFmpegExporterClient.ts
@@ -1,28 +1,30 @@
-import type {Project, RendererSettings} from '@motion-canvas/core';
-import {Exporter} from '@motion-canvas/core/lib/app';
+import type { Project, RendererSettings } from '@motion-canvas/core';
+import { Exporter } from '@motion-canvas/core/lib/app';
 import {
-  BoolMetaField,
-  ObjectMetaField,
-  ValueOf,
+	BoolMetaField,
+	EnumMetaField,
+	ObjectMetaField,
+	StringMetaField,
+	ValueOf,
 } from '@motion-canvas/core/lib/meta';
-import type {RendererResult} from '@motion-canvas/core';
-import type {MetaField} from '@motion-canvas/core/lib/meta';
-import {EventDispatcher} from '@motion-canvas/core/lib/events';
+import type { RendererResult } from '@motion-canvas/core';
+import type { MetaField } from '@motion-canvas/core/lib/meta';
+import { EventDispatcher } from '@motion-canvas/core/lib/events';
 
 type ServerResponse =
-  | {
-      status: 'success';
-      method: string;
-      data: unknown;
-    }
-  | {
-      status: 'error';
-      method: string;
-      message?: string;
-    };
+	| {
+		status: 'success';
+		method: string;
+		data: unknown;
+	}
+	| {
+		status: 'error';
+		method: string;
+		message?: string;
+	};
 
 type FFmpegExporterOptions = ValueOf<
-  ReturnType<typeof FFmpegExporterClient.meta>
+	ReturnType<typeof FFmpegExporterClient.meta>
 >;
 
 /**
@@ -44,93 +46,104 @@ type FFmpegExporterOptions = ValueOf<
  * initialized by invoking `start`.
  */
 export class FFmpegExporterClient implements Exporter {
-  public static readonly id = '@motion-canvas/ffmpeg-edited-transparency';
-  public static readonly displayName = 'Video With Transparency (FFmpeg)';
+	public static readonly id = '@motion-canvas/ffmpeg-edited-transparency';
+	public static readonly displayName = 'Video With Transparency (FFmpeg)';
 
-  public static meta(project: Project): MetaField<any> {
-    return new ObjectMetaField(this.displayName, {
-      fastStart: new BoolMetaField('fast start', true),
-      includeAudio: new BoolMetaField('include audio', true).disable(
-        !project.audio,
-      ),
-    });
-  }
+	public static meta(project: Project): MetaField<any> {
+		return new ObjectMetaField(this.displayName, {
+			fastStart: new BoolMetaField('fast start', true),
+			includeAudio: new BoolMetaField('include audio', true).disable(
+				!project.audio,
+			),
+			fileType: new EnumMetaField("file type", [
+				{ value: "webm", text: "webm" },
+				{ value: "mov", text: "mov" },
+				{ value: "mp4", text: "mp4" },
+			], "webm"),
+			pixelFormat: new EnumMetaField("file type", [
+				{ value: "rgba", text: "rgba" },
+				{ value: "yuv420p", text: "yuv420p" },
+				{ value: "yuva420p", text: "yuva420p" },
+				{ value: "yuv411p", text: "yuv411p" }
+			],"rgba"),
+		});
+	}
 
-  public static async create(project: Project, settings: RendererSettings) {
-    return new FFmpegExporterClient(project, settings);
-  }
+	public static async create(project: Project, settings: RendererSettings) {
+		return new FFmpegExporterClient(project, settings);
+	}
 
-  private static readonly response = new EventDispatcher<ServerResponse>();
+	private static readonly response = new EventDispatcher<ServerResponse>();
 
-  static {
-    if (import.meta.hot) {
-      import.meta.hot.on(
-        `motion-canvas/ffmpeg-ack-edited-transparency`,
-        (response: ServerResponse) => this.response.dispatch(response),
-      );
-    }
-  }
+	static {
+		if (import.meta.hot) {
+			import.meta.hot.on(
+				`motion-canvas/ffmpeg-ack-edited-transparency`,
+				(response: ServerResponse) => this.response.dispatch(response),
+			);
+		}
+	}
 
-  public constructor(
-    private readonly project: Project,
-    private readonly settings: RendererSettings,
-  ) {}
+	public constructor(
+		private readonly project: Project,
+		private readonly settings: RendererSettings,
+	) { }
 
-  public async start(): Promise<void> {
-    const options = this.settings.exporter.options as FFmpegExporterOptions;
-    await this.invoke('start', {
-      ...this.settings,
-      ...options,
-      audio: this.project.audio,
-      audioOffset:
-        this.project.meta.shared.audioOffset.get() - this.settings.range[0],
-    });
-  }
+	public async start(): Promise<void> {
+		const options = this.settings.exporter.options as FFmpegExporterOptions;
+		await this.invoke('start', {
+			...this.settings,
+			...options,
+			audio: this.project.audio,
+			audioOffset:
+				this.project.meta.shared.audioOffset.get() - this.settings.range[0],
+		});
+	}
 
-  public async handleFrame(canvas: HTMLCanvasElement): Promise<void> {
-    await this.invoke('handleFrame', {
-      data: canvas.toDataURL('image/png'),
-    });
-  }
+	public async handleFrame(canvas: HTMLCanvasElement): Promise<void> {
+		await this.invoke('handleFrame', {
+			data: canvas.toDataURL('image/png'),
+		});
+	}
 
-  public async stop(result: RendererResult): Promise<void> {
-    await this.invoke('end', result);
-  }
+	public async stop(result: RendererResult): Promise<void> {
+		await this.invoke('end', result);
+	}
 
-  /**
-   * Remotely invoke a method on the server and wait for a response.
-   *
-   * @param method - The method name to execute on the server.
-   * @param data - The data that will be passed as an argument to the method.
-   *               Should be serializable.
-   */
-  private invoke<TResponse = unknown, TData = unknown>(
-    method: string,
-    data: TData,
-  ): Promise<TResponse> {
-    if (import.meta.hot) {
-      return new Promise((resolve, reject) => {
-        const handle = (response: ServerResponse) => {
-          if (response.method !== method) {
-            return;
-          }
+	/**
+	 * Remotely invoke a method on the server and wait for a response.
+	 *
+	 * @param method - The method name to execute on the server.
+	 * @param data - The data that will be passed as an argument to the method.
+	 *               Should be serializable.
+	 */
+	private invoke<TResponse = unknown, TData = unknown>(
+		method: string,
+		data: TData,
+	): Promise<TResponse> {
+		if (import.meta.hot) {
+			return new Promise((resolve, reject) => {
+				const handle = (response: ServerResponse) => {
+					if (response.method !== method) {
+						return;
+					}
 
-          FFmpegExporterClient.response.unsubscribe(handle);
-          if (response.status === 'success') {
-            resolve(response.data as TResponse);
-          } else {
-            reject({
-              message: 'An error occurred while exporting the video.',
-              remarks: `Method: ${method}<br>Server error: ${response.message}`,
-              object: data,
-            });
-          }
-        };
-        FFmpegExporterClient.response.subscribe(handle);
-        import.meta.hot!.send('motion-canvas/ffmpeg-edited-transparency', {method, data});
-      });
-    } else {
-      throw new Error('FFmpegExporter can only be used locally.');
-    }
-  }
+					FFmpegExporterClient.response.unsubscribe(handle);
+					if (response.status === 'success') {
+						resolve(response.data as TResponse);
+					} else {
+						reject({
+							message: 'An error occurred while exporting the video.',
+							remarks: `Method: ${method}<br>Server error: ${response.message}`,
+							object: data,
+						});
+					}
+				};
+				FFmpegExporterClient.response.subscribe(handle);
+				import.meta.hot!.send('motion-canvas/ffmpeg-edited-transparency', { method, data });
+			});
+		} else {
+			throw new Error('FFmpegExporter can only be used locally.');
+		}
+	}
 }
--- packages/ffmpeg-edited-transparency/server/FFmpegExporterServer.ts
+++ packages/ffmpeg-edited-transparency/server/FFmpegExporterServer.ts
@@ -1,12 +1,12 @@
-import {ImageStream} from './ImageStream';
-import type {PluginConfig} from '@motion-canvas/vite-plugin/lib/plugins';
+import { ImageStream } from './ImageStream';
+import type { PluginConfig } from '@motion-canvas/vite-plugin/lib/plugins';
 import type {
-  RendererSettings,
-  RendererResult,
+	RendererSettings,
+	RendererResult,
 } from '@motion-canvas/core/lib/app';
 import * as ffmpeg from 'fluent-ffmpeg';
-import {path as ffmpegPath} from '@ffmpeg-installer/ffmpeg';
-import {path as ffprobePath} from '@ffprobe-installer/ffprobe';
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
+import { path as ffprobePath } from '@ffprobe-installer/ffprobe';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -14,83 +14,85 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 ffmpeg.setFfprobePath(ffprobePath);
 
 export interface FFmpegExporterSettings extends RendererSettings {
-  audio?: string;
-  audioOffset?: number;
-  fastStart: boolean;
-  includeAudio: boolean;
+	audio?: string;
+	audioOffset?: number;
+	fastStart: boolean;
+	includeAudio: boolean;
+	fileType: "webm" | "mov" | "mp4";
+	pixelFormat: "rgba" | "yuv420p" | "yuva420p" | "yuv411p";
 }
 
 /**
  * The server-side implementation of the FFmpeg video exporter.
  */
 export class FFmpegExporterServer {
-  private readonly stream: ImageStream;
-  private readonly command: ffmpeg.FfmpegCommand;
-  private readonly promise: Promise<void>;
+	private readonly stream: ImageStream;
+	private readonly command: ffmpeg.FfmpegCommand;
+	private readonly promise: Promise<void>;
 
-  public constructor(
-    settings: FFmpegExporterSettings,
-    private readonly config: PluginConfig,
-  ) {
-    this.stream = new ImageStream();
-    this.command = ffmpeg();
+	public constructor(
+		settings: FFmpegExporterSettings,
+		private readonly config: PluginConfig,
+	) {
+		this.stream = new ImageStream();
+		this.command = ffmpeg();
 
-    // Input image sequence
-    this.command
-      .input(this.stream)
-      .inputFormat('image2pipe')
-      .inputFps(settings.fps);
+		// Input image sequence
+		this.command
+			.input(this.stream)
+			.inputFormat('image2pipe')
+			.inputFps(settings.fps);
 
-    // Input audio file
-    if (settings.includeAudio && settings.audio) {
-      this.command
-        .input((settings.audio as string).slice(1))
-        // FIXME Offset only works for negative values.
-        .inputOptions([`-itsoffset ${settings.audioOffset ?? 0}`]);
-    }
+		// Input audio file
+		if (settings.includeAudio && settings.audio) {
+			this.command
+				.input((settings.audio as string).slice(1))
+				// FIXME Offset only works for negative values.
+				.inputOptions([`-itsoffset ${settings.audioOffset ?? 0}`]);
+		}
 
-    // Output settings
-    const size = {
-      x: Math.round(settings.size.x * settings.resolutionScale),
-      y: Math.round(settings.size.y * settings.resolutionScale),
-    };
-    this.command
-      .output(path.join(this.config.output, `${settings.name}.webm`))
-      .outputOptions(['-pix_fmt rgba', '-shortest'])
-      .outputFps(settings.fps)
-      .size(`${size.x}x${size.y}`);
-    if (settings.fastStart) {
-      this.command.outputOptions(['-movflags +faststart']);
-    }
+		// Output settings
+		const size = {
+			x: Math.round(settings.size.x * settings.resolutionScale),
+			y: Math.round(settings.size.y * settings.resolutionScale),
+		};
+		this.command
+			.output(path.join(this.config.output, `${settings.name}.${settings.fileType}`))
+			.outputOptions([`-pix_fmt ${settings.pixelFormat}`, '-shortest'])
+			.outputFps(settings.fps)
+			.size(`${size.x}x${size.y}`);
+		if (settings.fastStart) {
+			this.command.outputOptions(['-movflags +faststart']);
+		}
 
-    this.promise = new Promise<void>((resolve, reject) => {
-      this.command.on('end', resolve).on('error', reject);
-    });
-  }
+		this.promise = new Promise<void>((resolve, reject) => {
+			this.command.on('end', resolve).on('error', reject);
+		});
+	}
 
-  public async start() {
-    if (!fs.existsSync(this.config.output)) {
-      await fs.promises.mkdir(this.config.output, {recursive: true});
-    }
-    this.command.run();
-  }
+	public async start() {
+		if (!fs.existsSync(this.config.output)) {
+			await fs.promises.mkdir(this.config.output, { recursive: true });
+		}
+		this.command.run();
+	}
 
-  public async handleFrame({data}: {data: string}) {
-    const base64Data = data.slice(data.indexOf(',') + 1);
-    this.stream.pushImage(Buffer.from(base64Data, 'base64'));
-  }
+	public async handleFrame({ data }: { data: string }) {
+		const base64Data = data.slice(data.indexOf(',') + 1);
+		this.stream.pushImage(Buffer.from(base64Data, 'base64'));
+	}
 
-  public async end(result: RendererResult) {
-    this.stream.pushImage(null);
-    if (result === 1) {
-      try {
-        this.command.kill('SIGKILL');
-        await this.promise;
-      } catch (_) {
-        // do nothing
-      }
-    } else {
-      await this.promise;
-    }
-  }
+	public async end(result: RendererResult) {
+		this.stream.pushImage(null);
+		if (result === 1) {
+			try {
+				this.command.kill('SIGKILL');
+				await this.promise;
+			} catch (_) {
+				// do nothing
+			}
+		} else {
+			await this.promise;
+		}
+	}
 }
